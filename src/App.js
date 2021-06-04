f0db92fcf31e291f0ecd01c5a5e65ebccb0b3e52
asitsi <41507591+asitsi@users.noreply.github.com>
2021-06-04 14:48:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Switch>
-          <Route path="/teslaClone" exact component={Home}></Route>
+          <Route path="/" exact component={Home}></Route>
         </Switch>
       </BrowserRouter>
     </div>
